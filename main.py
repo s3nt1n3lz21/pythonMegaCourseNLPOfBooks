a055052ca038ac94460c1fd7485f092fb1438cf3
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-10 14:48:56 +0100

    Now finding the most used words

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import re
+from nltk.corpus import stopwords
+
+english_stopwords = stopwords.words("english")
+
+def is_not_stopword(x):
+    return x not in english_stopwords
 
 with open("miracle_in_the_andes.txt", "r", encoding="utf-8") as file:
     book = file.read()
@@ -15,11 +21,10 @@ with open("miracle_in_the_andes.txt", "r", encoding="utf-8") as file:
     # What are the most used words
     pattern = re.compile("[a-zA-Z]+")
     words = re.findall(pattern, book.lower())
-    print(words)
-    print(len(words))
-    # list_of_words = book.replace(".","").replace(",","").replace(":","").lower().split( )
+    words_filtered = filter(is_not_stopword, words)
+
     words_dict = dict()
-    for word in words:
+    for word in words_filtered:
         number_occurences = words_dict.get(word)
         if number_occurences:
             words_dict[word] = number_occurences + 1
@@ -27,4 +32,4 @@ with open("miracle_in_the_andes.txt", "r", encoding="utf-8") as file:
             words_dict[word] = 1
 
     sorted_words_dict = sorted(words_dict.items(), key=lambda x:x[1])
-    print(sorted_words_dict)
+    # print(sorted_words_dict)
