281cae73a8324b4f5905b861818dfefa3e3662d9
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-10 15:20:17 +0100

    Now showing charts of positivity and negativity in each chapter of the book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import re
 from nltk.corpus import stopwords
+from nltk.sentiment import SentimentIntensityAnalyzer
+import streamlit as st
+import plotly.express as px
 
 english_stopwords = stopwords.words("english")
 
@@ -33,3 +36,19 @@ with open("miracle_in_the_andes.txt", "r", encoding="utf-8") as file:
 
     sorted_words_dict = sorted(words_dict.items(), key=lambda x:x[1])
     # print(sorted_words_dict)
+
+    # What are the most positive and negative chapters
+    analyser = SentimentIntensityAnalyzer()
+
+    pattern = re.compile("Chapter [0-9]+")
+    chapters = re.split(pattern, book)
+    chapters = chapters[1:]
+    scores = [analyser.polarity_scores(chapter) for chapter in chapters]
+    print(scores)
+    positive_scores = [score.get('pos') for score in scores]
+    negative_scores = [score.get('neg') for score in scores]
+
+    figure = px.line(x=range(1,11), y=positive_scores, labels={"x": "Chapter", "y": "Score"})
+    st.plotly_chart(figure)
+    figure = px.line(x=range(1, 11), y=negative_scores, labels={"x": "Chapter", "y": "Score"})
+    st.plotly_chart(figure)
